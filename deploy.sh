b491280d1c0d5a7642c54e4bf3fa06edd37eec52
Dhole <dhole@openmailbox.org>
2018-10-12 23:42:33 +0200

    rebuilding site Fri Oct 12 23:42:33 CEST 2018

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,8 @@ git push origin hugo
 git subtree push --prefix=public git@github.com:Dhole/dhole.github.io.git master
 
 # Rebuild for my domain and push to my git repo
-hugo -t liquorice_mod -b https://lizard.kyasuka.com/blog/
+#hugo -t liquorice_mod -b https://lizard.kyasuka.com/blog/
+hugo -t liquorice_mod -b https://lizard.niobe.cc/blog/
 git add -A
 git commit -m "$msg"
 git push lizard hugo
